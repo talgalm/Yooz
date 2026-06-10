93401aff4ec1810d2e8b3c91f57f0b1980ef6849
TalGalmor <talgalm@users.noreply.github.com>
2026-06-10 12:00:09 +0300

    fix puzzle part

--- client/src/pages/MissionPage/MissionPuzzle.tsx
+++ client/src/pages/MissionPage/MissionPuzzle.tsx
@@ -173,7 +173,7 @@ const GridArea = styled('div')({
   position: 'relative',
   width: '85vw',
   maxWidth: 380,
-  marginTop: '18%',
+  marginTop: '30%',
   aspectRatio: '1 / 1',
   borderRadius: 4,
   overflow: 'hidden',
